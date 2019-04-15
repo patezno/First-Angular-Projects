3497f5c9126f648e098bdeca688d4f648d05902f
Carlos Alhama <patezno97@gmail.com>
2019-04-15 21:40:57 +0200

    buscador funciona

--- 02-spa/src/app/app.module.ts
+++ 02-spa/src/app/app.module.ts
@@ -14,6 +14,8 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroeComponent } from './components/heroe/heroe.component';
+import { SearcherComponent } from './components/searcher/searcher.component';
+import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
 
 
 @NgModule({
@@ -23,7 +25,9 @@ import { HeroeComponent } from './components/heroe/heroe.component';
     HomeComponent,
     AboutComponent,
     HeroesComponent,
-    HeroeComponent
+    HeroeComponent,
+    SearcherComponent,
+    HeroeTarjetaComponent
   ],
   imports: [
     BrowserModule,
