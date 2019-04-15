450b5c55cfb68359e497717006fde111d1c310eb
Carlos Alhama <patezno97@gmail.com>
2019-04-15 21:54:32 +0200

    modulo heroes reutilizable

--- 02-spa/src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
+++ 02-spa/src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-heroe-tarjeta',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroeTarjetaComponent implements OnInit {
 
-  constructor() { }
+  @Input() heroe: any = {};
+  @Input() index: number;
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  verHeroe() {
+    this.router.navigate(['/heroe', this.index]);
   }
 
 }
