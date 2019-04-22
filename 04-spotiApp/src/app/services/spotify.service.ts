cedf5099b68804d0e8f1c20fae0f8887fa94c49a
Carlos Alhama <patezno97@gmail.com>
2019-04-22 18:48:50 +0200

    servicio hacia API spotify creado

---
+++ 04-spotiApp/src/app/services/spotify.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpotifyService {
+
+  constructor(private http: HttpClient) { }
+
+  getNewReleases() {
+
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer BQApTwoo-sNKKx7JiQL5K66IQW4zi-TPtjpAoz_JASMqNQ14zOufDO7L9IaLNCup6Jzn5gzadvoMLrqnTCo'
+    });
+
+    this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', {headers})
+    .subscribe(data => {
+      console.log(data);
+    });
+  }
+}
