b2af00bb91c6823fd328d5148fec0432e88ac48d
Carlos Alhama <patezno97@gmail.com>
2019-04-22 18:25:54 +0200

    prueba REST realizada

--- 04-spotiApp/src/app/components/home/home.component.ts
+++ 04-spotiApp/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  paises: any[] = [];
+
+  constructor(private http: HttpClient) {
+    this.http.get('https://restcountries.eu/rest/v2/lang/es')
+    .subscribe((data: any) => {
+      this.paises = data;
+    })
+   }
 
   ngOnInit() {
   }
