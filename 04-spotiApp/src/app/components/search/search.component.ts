8d5591eda0a834901d21a55967ef8f5c0ae4c892
Carlos Alhama <patezno97@gmail.com>
2019-05-01 19:52:56 +0200

    mensaje error mostrado en search

--- 04-spotiApp/src/app/components/search/search.component.ts
+++ 04-spotiApp/src/app/components/search/search.component.ts
@@ -10,18 +10,22 @@ import { SpotifyService } from '../../services/spotify.service';
 export class SearchComponent {
 
   artistas: any[] = [];
-
   loading: boolean;
+  error: boolean;
 
   constructor(private spotify: SpotifyService) {}
 
   buscar(termino: string) {
 
     this.loading = true;
+    this.error = false;
 
     this.spotify.getArtistas(termino).subscribe(data => {
       this.artistas = data;
       this.loading = false;
+    }, error => {
+      this.loading = false;
+      this.error = true;
     });
   }
 
