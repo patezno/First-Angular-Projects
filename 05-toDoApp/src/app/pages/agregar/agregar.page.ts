16fa46685cc989f063e05fdb44a6eb72233545f9
Carlos Alhama Gil <carlos.alhamagil@plexus.es>
2019-08-13 13:35:50 +0200

    se pueden moficiar listas creadas

--- 05-toDoApp/src/app/pages/agregar/agregar.page.ts
+++ 05-toDoApp/src/app/pages/agregar/agregar.page.ts
@@ -50,4 +50,9 @@ export class AgregarPage implements OnInit {
     this.deseosService.guardarStorage();
   }
 
+  borrar(i: number) {
+    this.lista.items.splice(i, 1);
+    this.deseosService.guardarStorage();
+  }
+
 }
