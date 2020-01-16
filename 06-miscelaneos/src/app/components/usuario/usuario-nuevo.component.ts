2d95f8b81fa17d1c3f336ffeea61b2a2809c32e1
carlos <carlos.alhamagil@plexus.es>
2020-01-16 12:02:24 +0100

    obtencion de parametros del router padre

--- 06-miscelaneos/src/app/components/usuario/usuario-nuevo.component.ts
+++ 06-miscelaneos/src/app/components/usuario/usuario-nuevo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-usuario-nuevo',
@@ -11,7 +12,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsuarioNuevoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute) {
+    this.activatedRoute.parent.params.subscribe(parametros => {
+      console.log('Ruta hija user nuevo');
+      console.log(parametros);
+    });
+  }
 
   ngOnInit() {
   }
