e8b2832fbadbc4a173266af9910e17e04914e38f
carlos <carlos.alhamagil@plexus.es>
2020-01-17 10:34:03 +0100

    form capturado

--- 07-bonus-loginApp/src/app/pages/registro/registro.component.ts
+++ 07-bonus-loginApp/src/app/pages/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from '../../models/usuario.model';
 
 @Component({
   selector: 'app-registro',
@@ -7,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
+  usuario: Usuario;
 
   constructor() { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.usuario = new Usuario();
+    this.usuario.email = 'patezno97@gmail.com';
+  }
+
+  onSubmit() {
+    console.log('Formulario enviado', this.usuario);
+  }
 
 
 }
