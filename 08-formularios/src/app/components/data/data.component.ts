c27b0b98e8181cec49f21086f7c15ed2e67d02a6
carlos <carlos.alhamagil@plexus.es>
2020-01-23 13:14:57 +0100

    codigo copiado

--- 08-formularios/src/app/components/data/data.component.ts
+++ 08-formularios/src/app/components/data/data.component.ts
@@ -1,31 +1,149 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
   styles: []
 })
-export class DataComponent implements OnInit {
+export class DataComponent {
 
-  form: FormGroup;
+  forma: FormGroup;
 
-  constructor() { }
+  usuario = {
 
-  ngOnInit() {
-    this.buildForm();
-  }
+    nombrecompleto: {
+      nombre: 'fernando',
+      apellido: 'herrera'
+    },
+    correo: 'fernando.herrera85@gmail.com',
+    // pasatiempos: ["Correr","Dormir","Comer"]
+  };
 
-  buildForm() {
-    this.form = new FormGroup({
-      nombre: new FormControl('', Validators.required),
-      apellido: new FormControl('', Validators.required),
-      correo: new FormControl('', Validators.email)
+  constructor() {
+
+    console.log(this.usuario);
+
+    this.forma = new FormGroup({
+
+      nombrecompleto: new FormGroup({
+        nombre: new FormControl('' ,  [
+                                          Validators.required,
+                                          Validators.minLength(3)
+                                        ]),
+        apellido: new FormControl('', [
+                                          Validators.required,
+                                          this.noHerrera
+                                        ])
+      }),
+      correo: new FormControl('',   [
+                                        Validators.required,
+                                        Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')
+                                      ]),
+      pasatiempos: new FormArray([
+        new FormControl('Correr', Validators.required )
+      ]),
+      username: new FormControl('', Validators.required, this.existeUsuario  ),
+      password1: new FormControl('', Validators.required),
+      password2: new FormControl()
     });
+
+    // this.forma.setValue( this.usuario );
+
+    this.forma.controls.password2.setValidators([
+      Validators.required,
+      this.noIgual.bind( this.forma )
+    ]);
+
+
+    this.forma.controls.username.valueChanges
+        .subscribe(  data => {
+          console.log(data);
+        });
+
+    this.forma.controls.username.statusChanges
+        .subscribe(  data => {
+          console.log(data);
+          });
+
+
+
   }
+
+
+  agregarPasatiempo() {
+    (this.forma.controls.pasatiempos as FormArray).push(
+      new FormControl('', Validators.required )
+    );
+  }
+
+
+  noHerrera( control: FormControl ): { [s: string]: boolean }  {
+
+    if ( control.value === 'herrera' ) {
+      return {
+        noherrera: true
+      };
+    }
+
+    return null;
+
+  }
+
+  noIgual( control: FormControl ): { [s: string]: boolean }  {
+
+    // console.log(this);
+    const forma: any = this;
+
+    if ( control.value !== forma.controls.password1.value ) {
+      return {
+        noiguales: true
+      };
+    }
+
+    return null;
+
+  }
+
+  existeUsuario( control: FormControl ): Promise<any>|Observable<any> {
+
+    const promesa = new Promise(
+      ( resolve, reject ) => {
+
+        setTimeout( () => {
+          if ( control.value === 'strider' ) {
+            resolve( {existe: true} );
+          } else {
+            resolve( null );
+          }
+
+        }, 3000 );
+
+      }
+    );
+
+    return promesa;
+
+  }
+
+
 
   guardarCambios() {
-    console.log(this.form.value);
+    console.log( this.forma.value );
+    console.log( this.forma );
+
+    // this.forma.reset( {
+    //   nombrecompleto:{
+    //     nombre: "",
+    //     apellido: ""
+    //   },
+    //   correo:""
+    // });
+
+
   }
+
 
 }
