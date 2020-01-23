fab57175e3d01b913f2966508ab7cef96fbd82d1
carlos <carlos.alhamagil@plexus.es>
2020-01-23 12:36:27 +0100

    formgroup

--- 08-formularios/src/app/components/data/data.component.ts
+++ 08-formularios/src/app/components/data/data.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-data',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataComponent implements OnInit {
 
+  form: FormGroup;
+
   constructor() { }
 
   ngOnInit() {
+    this.buildForm();
+  }
+
+  buildForm() {
+    this.form = new FormGroup({
+      nombre: new FormControl('Carlos'),
+      apellido: new FormControl(),
+      correo: new FormControl()
+    });
+  }
+
+  guardarCambios() {
+    console.log(this.form.value);
   }
 
 }
