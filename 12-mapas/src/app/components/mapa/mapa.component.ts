c438f717e12ed2979067ee79ba50ffde544c1108
carlos <carlos.alhamagil@plexus.es>
2020-01-23 09:46:37 +0100

    marcadores añadidos con click

--- 12-mapas/src/app/components/mapa/mapa.component.ts
+++ 12-mapas/src/app/components/mapa/mapa.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Marcador } from '../../classes/marcador.class';
 
 @Component({
   selector: 'app-mapa',
@@ -7,12 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapaComponent implements OnInit {
 
+  marcadores: Marcador[] = [];
+
   lat = 51.678418;
   lng = 7.809007;
 
-  constructor() { }
+  constructor() {
+    const marcador = new Marcador(51.678418, 7.809007);
+    this.marcadores.push(marcador);
+  }
 
   ngOnInit() {
   }
 
+  agregarMarcador(event: any) {
+    const coords = event.coords;
+    const marcador = new Marcador(coords.lat, coords.lng);
+    this.marcadores.push(marcador);
+
+  }
 }
