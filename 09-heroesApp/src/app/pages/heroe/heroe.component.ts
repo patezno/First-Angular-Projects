191593b10e12b38334e21c8eafa902c35f5b05c4
carlos <carlos.alhamagil@plexus.es>
2020-01-27 11:54:05 +0100

    optimizacion

--- 09-heroesApp/src/app/pages/heroe/heroe.component.ts
+++ 09-heroesApp/src/app/pages/heroe/heroe.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Heroe } from '../../models/heroe.model';
 import { NgForm } from '@angular/forms';
 import { HeroesService } from '../../services/heroes.service';
+import Swal from 'sweetalert2';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-heroe',
@@ -22,16 +24,29 @@ export class HeroeComponent implements OnInit {
       return;
     }
 
+    Swal.fire({
+      title: 'Espere',
+      text: 'Guardando información',
+      icon: 'info',
+      allowOutsideClick: false
+    });
+    Swal.showLoading();
+
+    let peticion: Observable<any>;
+
     if (this.heroe.id) {
-      this.heroesService.actualizarHeroe(this.heroe).subscribe(resp => {
-        console.log(resp);
-      });
+      peticion = this.heroesService.actualizarHeroe(this.heroe);
     } else {
-      this.heroesService.crearHeroe(this.heroe).subscribe(resp => {
-        console.log(resp);
-        this.heroe = resp;
-      });
+      peticion = this.heroesService.crearHeroe(this.heroe);
     }
+
+    peticion.subscribe(resp => {
+      Swal.fire({
+        title: this.heroe.nombre,
+        text: 'Se actualizó correctamente',
+        icon: 'success'
+      });
+    });
   }
 
 }
