030cd622280d2667294b8b8586e8281f29886f1a
carlos <carlos.alhamagil@plexus.es>
2020-02-03 13:51:47 +0100

    autenticacion google añadida

--- 10-firechat/src/app/services/chat.service.ts
+++ 10-firechat/src/app/services/chat.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { Mensaje } from '../models/mensaje.model';
 import { map } from 'rxjs/operators';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +12,25 @@ export class ChatService {
 
   private itemsCollection: AngularFirestoreCollection<any>;
   public chats: Mensaje[] = [];
+  public usuario: any = {};
 
-  constructor(private afs: AngularFirestore) { }
+  constructor(private afs: AngularFirestore, public afAuth: AngularFireAuth) {
+    afAuth.authState.subscribe(user => {
+      if (!user) {
+        return;
+      }
+      this.usuario.nombre = user.displayName;
+      this.usuario.uid = user.uid;
+    });
+  }
+
+  login(metodo: string) {
+    this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
+  }
 
   cargarMensajes() {
     this.itemsCollection = this.afs.collection<Mensaje>('chats', ref => ref.orderBy('fecha', 'desc').limitToLast(10));
