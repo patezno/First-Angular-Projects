a5608c65f3b65342b6186a717c6c45f95a88259e
carlos <carlos.alhamagil@plexus.es>
2020-02-04 12:51:37 +0100

    app finalizada

--- 10-firechat/src/app/services/chat.service.ts
+++ 10-firechat/src/app/services/chat.service.ts
@@ -50,9 +50,10 @@ export class ChatService {
 
   agregarMensaje(texto: string) {
     const mensaje: Mensaje = {
-      nombre: 'Demo',
+      nombre: this.usuario.nombre,
       mensaje: texto,
-      fecha: new Date().getTime()
+      fecha: new Date().getTime(),
+      uid: this.usuario.uid
     };
     return this.itemsCollection.add(mensaje);
   }
