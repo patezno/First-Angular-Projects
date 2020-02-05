9b07579f57a5b663698a15afe0d46843d736b4c1
carlos <carlos.alhamagil@plexus.es>
2020-02-05 13:19:55 +0100

    maquetacion home

--- 11-peliculasApp/src/app/app.component.ts
+++ 11-peliculasApp/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { PeliculasService } from './services/peliculas.service';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +6,6 @@ import { PeliculasService } from './services/peliculas.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'peliculasApp';
 
-  constructor(public peliculasService: PeliculasService) {
-    this.peliculasService.getPopulares().subscribe(data => console.log(data));
-  }
+  constructor() {}
 }
--- 11-peliculasApp/src/app/services/peliculas.service.ts
+++ 11-peliculasApp/src/app/services/peliculas.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import * as moment from 'moment';
 
 @Injectable({
   providedIn: 'root'
@@ -19,6 +20,13 @@ export class PeliculasService {
 
   buscarPeliculas(text: string) {
     const url = `${this.urlMoviedb}search/movie?query=${text}&sort_by=popularity.desc&api_key=${this.apikey}&language=es`;
+    return this.http.get(url).pipe(map((res: any) => res.results));
+  }
+
+  getCartelera() {
+    const startDate = moment().add(-15, 'days').format('Y-MM-DD');
+    const finishDate = moment().format('Y-MM-DD');
+    const url = `${this.urlMoviedb}discover/movie?primary_release_date.gte=${startDate}&primary_release_date.lte=${finishDate}&api_key=${this.apikey}&language=es`;
     return this.http.get(url).pipe(map((res: any) => res.results));
   }
 }
