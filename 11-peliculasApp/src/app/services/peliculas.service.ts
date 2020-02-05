70969575b5ef5f6cb16dab535772ad9e0770fa08
carlos <carlos.alhamagil@plexus.es>
2020-02-05 12:17:20 +0100

    proyecto peliculas iniciado

---
+++ 11-peliculasApp/src/app/services/peliculas.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PeliculasService {
+
+  private apikey = '6b6b33eb4942fd739eaf2a8a4d671e17';
+  private urlMoviedb = 'https://api.themoviedb.org/3/';
+
+  constructor(private http: HttpClient) { }
+
+  getPopulares() {
+
+    const url = `${this.urlMoviedb}discover/movie?sort_by=popularity.desc&api_key=${this.apikey}&language=es`;
+
+    return this.http.get(url)
+                    .pipe(map((res: any) => res.results));
+  }
+}
